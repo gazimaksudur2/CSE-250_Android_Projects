c48a20a86a51362490866c66b9fa8ff5874941de
GaziMaksudur <gazimaksudur2@gmail.com>
2023-10-30 13:53:15 +0600

    initial commit for Android project

---
+++ Sticky_Note_Application/app/src/main/java/com/example/sticky_note_application/StickyNote.java
@@ -0,0 +1,47 @@
+package com.example.sticky_note_application;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class StickyNote {
+
+    String getstick(Context context){
+        File file = new File(context.getFilesDir().getPath()+"gfg.txt");
+        StringBuilder text = new StringBuilder();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while((line = br.readLine())!= null){
+                text.append(line);
+                text.append("\n");
+            }
+            br.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return text.toString();
+    }
+    void setStick(String textTobeSaved, Context context){
+        String text = textTobeSaved;
+        FileOutputStream fos = null;
+        try{
+            fos = context.getApplicationContext().openFileOutput("gfg.txt",Context.MODE_PRIVATE);
+            fos.write(text.getBytes());
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            if(fos != null){
+                try{
+                    fos.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
